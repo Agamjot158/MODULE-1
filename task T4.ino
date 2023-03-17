857e5f947370c3d71b2b85d918152edc85a37b09
Agamjot158 <106953882+Agamjot158@users.noreply.github.com>
2023-03-17 14:18:35 +0530

    Update task T4.ino

--- task T4.ino
+++ task T4.ino
@@ -2,7 +2,7 @@ const byte Led = 12;
 const byte timer_Led = 10;
 const byte button1 = 2;
 const byte button2 = 3;
-const byte button3 = 4;
+const byte button3 = 8;
 bool LedState = false;
 int timer1_compare_match = 31249;
 
@@ -19,6 +19,8 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt (button1), button_1, CHANGE);
   attachInterrupt(digitalPinToInterrupt(button2), button_2, CHANGE);
  
+  PCICR |= B00000001;
+  PCMSK0 |= B00000001;
  
   TCCR1A = 0;   
   TCCR1B = 0;   
@@ -42,7 +44,6 @@ void loop() {
 void button_2()
 {
   Serial.println("Interrupt detected: Button #2 ");
-  Serial.println("");
   LedState = !LedState;
   digitalWrite(Led, LedState);
 }
@@ -52,12 +53,10 @@ void button_1()
   Serial.println("Interrupt detected: Button #1");
   LedState = !LedState;
   digitalWrite(Led, LedState);
-  Serial.println("");
-  Serial.println("Interrupt detected: Button #3");
   
 }
 
-void Interrupt3(){
+ISR(PCINT0_vect){
   
   Serial.println("Interrupt detected: Button #3");
   LedState = !LedState;
