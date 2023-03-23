a5a0cb18456d8a7c972c731ec2a7f6b13e4b4d43
Agamjot158 <106953882+Agamjot158@users.noreply.github.com>
2023-03-23 11:02:12 +0530

    Update task T4.ino

--- task T4.ino
+++ task T4.ino
@@ -1,10 +1,11 @@
 const byte Led = 12;
 const byte timer_Led = 10;
-const byte button1 = 2;
-const byte button2 = 3;
+const byte button1 = 4;
+const byte button2 = 5;
 const byte button3 = 8;
 bool LedState = false;
-int timer1_compare_match = 31249;
+int counter = 0;
+int timer1_compare_match = 20000; 
 
 void setup() {
  
@@ -19,8 +20,12 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt (button1), button_1, CHANGE);
   attachInterrupt(digitalPinToInterrupt(button2), button_2, CHANGE);
  
-  PCICR |= B00000001;
-  PCMSK0 |= B00000001;
+  
+  PCICR |= B00000101; // Enable interrupts on PD and PB port
+  PCMSK2 |= B00110000;
+  PCMSK0 |= B00000001;// Trigger interrupts on pins D4 and D5
+  
+   
  
   TCCR1A = 0;   
   TCCR1B = 0;   
@@ -62,9 +67,20 @@ ISR(PCINT0_vect){
   LedState = !LedState;
   digitalWrite(Led, LedState);
 }
+
+
   
-ISR(TIMER1_COMPA_vect)
-{
+  ISR(TIMER1_COMPA_vect){
   TCNT1 = timer1_compare_match;
   digitalWrite(timer_Led, digitalRead(timer_Led) ^ 1);
 }
+
+ISR (PCINT2_vect) {
+  if (B00010000) {
+    button_1();
+   
+  }
+  else if (B00100000) {
+    button_2();
+  }
+  }
